0982546fb3c622a9d5fc2947e76d782385e26f64
trhuyen33 <trthanhhuyen33@gmail.com>
2022-04-12 15:12:37 +0700

    src update

---
+++ lib/src/auth_widget.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:english_words/english_words.dart';
+
+class AuthWidget extends StatefulWidget {
+  final Widget child;
+  ///final void Function(AuthUser user)?
+  
+  const AuthWidget({
+    //required this.child, Key?key, this.
+  })
+
+  
+}
