719d1cb6f7a47be5215cf0affe0ae47fee0bef2f
trhuyen33 <trthanhhuyen33@gmail.com>
2022-04-12 15:53:53 +0700

    map widget

--- lib/src/auth_widget.dart
+++ lib/src/auth_widget.dart
@@ -4,11 +4,63 @@ import 'package:english_words/english_words.dart';
 
 class AuthWidget extends StatefulWidget {
   final Widget child;
-  ///final void Function(AuthUser user)?
-  
-  const AuthWidget({
-    //required this.child, Key?key, this.
-  })
+  final void Function(AuthUser user)? onUserSignedIn;
 
-  
+  const AuthWidget({
+    required this.child,
+    Key? key,
+    this.onUserSignedIn,
+  }) : super(key: key);
+
+  @override
+  State<AuthWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidget extends State<AuthWidget> {
+  @override
+  void initState() {
+    super.initState();
+    _signInAnonymously();
+  }
+
+  @override
+  Widget buid(BuildContext context) {
+    return widget.child;
+  }
+
+  Future<void> _signInAnonymously() async {
+    final userCredential = await FirebaseAuth.instance.signInAnonymously();
+    final user = userCredential.user;
+
+    if (user == null) {
+      debugPrint('_signInAnonymously: user is null');
+      return;
+    }
+
+    var isNew = false;
+    var name = user.displayName ?? '';
+    if (name.isEmpty) {
+      isNew = true;
+      final pair = generateWordPairs().take(1).first;
+      name = pair.asString;
+      debugPrint("_signInAnonymously: update ${user.uid}'s name $name");
+      await user.updateDisplayName(name);
+    } else {
+      debugPrint("_signInAnonymously: $name uid=${user.uid} signed in");
+    }
+
+    widget.onUserSignedIn?.call(AuthUser._(
+      isNew: isNew,
+      name: name,
+      uid: user.uid,
+    ));
+  }
+}
+
+class AuthUser {
+  final bool isNew;
+  final String name;
+  final String uid;
+
+  AuthUser._({required this.isNew, required this.name, required this.uid});
 }
---
+++ lib/src/map_widget.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:latlong2/latlong.dart';
+
+final _latLngBenThanh = LatLng(10.7721148, 106.6960897);
+
+class MapWidget extends StatefulWidget {
+  final LatLng? center;
+  final Iterable<LatLng>? markerPoints;
+
+  const MapWidget({this.center, Key? key, this.markerPoints}) : super(key: key);
+
+  @override
+  State<MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidget extends State<MapWidget> {
+  MapController? _controller;
+
+  @override
+  void didUpdateWidget(covariant MapWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    final center = widget.center;
+    final controller = _controller;
+    if (center != null && center != oldWidget.center && controller != null) {
+      controller.move(center, controller.zoom);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final center = widget.center;
+    final markerPoints = widget.markerPoints;
+
+    return FlutterMap(
+      layers: [
+        TileLayerOptions(
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c'],
+          attributionBuilder: (_) {
+            return const Text('© OpenStreetMap contributors');
+          },
+        ),
+        if (markerPoints != null)
+          MarkerClusterLayerOptions(
+            builder: (context, markers) {
+              return FloatingActionButton(
+                child: Text(markers.length.toString()),
+                onPressed: null,
+              );
+            },
+            markers: markerPoints
+                .where(
+                  (point) => point != center,
+                )
+                .map(
+                  (point) => Marker(
+                    width: _MarkerOther.size,
+                    height: _MarkerOther.size,
+                    point: point,
+                    builder: (_) => const _MarkerOther(),
+                  ),
+                )
+                .toList(growable: false),
+          ),
+        MarkerLayerOptions(
+          markers: [
+            if (center != null)
+              Marker(
+                width: _MarkerCenter.radius,
+                height: _MarkerCenter.radius,
+                point: center,
+                builder: (_) => const _MarkerCenter(),
+              ),
+          ],
+        ),
+      ],
+      options: MapOptions(
+        center: widget.center ?? _latLngBenThanh,
+        onMapCreated: _onMapCreated,
+        plugins: [
+          MarkerClusterPlugin(),
+        ],
+        zoom: 15.0,
+      ),
+    );
+  }
+
+  void _onMapCreated(MapController controller) {
+    _controller = controller;
+  }
+}
+
+class _MarkerCenter extends StatelessWidget {
+  static const radius = 18.0;
+
+  const _MarkerCenter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: const Color(0xFF0553B1),
+        ),
+        borderRadius: BorderRadius.circular(radius),
+        color: const Color(0xFF13B9FD),
+      ),
+    );
+  }
+}
+
+class _MarkerOther extends StatelessWidget {
+  static const size = 36.0;
+
+  const _MarkerOther({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/logos/flutter.png',
+      height: size,
+      width: size,
+    );
+  }
+}
